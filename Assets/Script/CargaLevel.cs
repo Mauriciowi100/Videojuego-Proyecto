4f49fd3764df1dc54d01894edb00e26e58ab8a5c
Mauriciowi <maumurillo@live.com.mx>
2020-10-20 20:34:20 -0700

    Menu y Nivel 1
    Se creo el menu con el boton iniciar juego que manda al nivel 1

---
+++ Assets/Script/CargaLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargaLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargarNivel (string NombreNivel)
+    {
+        SceneManager.LoadScene(NombreNivel);
+    }
+
+    public void salir()
+    {
+        Application.Quit();
+        Debug.Log("Saliste del juego");
+    }
+}
