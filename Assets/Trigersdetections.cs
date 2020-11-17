82cd865effe30ab4cbe79e407dc70451211ad8b3
Fernando P. Rodriguez <fernando.parra9903@gmail.com>
2020-11-17 16:35:19 -0700

    He añadido las llaves y el indicador en el panel

---
+++ Assets/Trigersdetections.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigersdetections : MonoBehaviour
+{
+
+    private int keys = 0;
+
+    public Text txtKeys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Key") {
+            Destroy(other.gameObject);
+            keys++;
+            txtKeys.text = keys.ToString();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Puerta") {
+            if (keys > 0)
+            {
+                Destroy(collision.gameObject);
+                keys--;
+                txtKeys.text = keys.ToString();
+            }
+            else {
+                Debug.Log("No tines una llave para pasr por aqui");
+            }
+        }
+        
+    }
+
+}
