3635089e16812cc164cfab2699c8fc9a89159063
gochoa032 <58501727+gochoa032@users.noreply.github.com>
2020-11-17 22:49:54 -0800

    Dialogo con sonidos, vegetacion y ambiente oscuro

--- Assets/Trigersdetections.cs
+++ Assets/Trigersdetections.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using SpeechLib;
+
 public class Trigersdetections : MonoBehaviour
 {
 
@@ -10,16 +12,36 @@ public class Trigersdetections : MonoBehaviour
 
     public Text txtKeys;
 
+    private SpVoice voice;
+    public GameObject panel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        voice = new SpVoice();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void mostrarDialogo(string txtMostrado, int duracion) {
+	IEnumerator waiter() {
+        panel.gameObject.SetActive(true);
+        panel.GetComponent<Text>().text = txtMostrado;
+        voice.Speak(txtMostrado, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+	yield return new WaitForSeconds(duracion); // espera
+	if (panel.GetComponent<Text>().text == txtMostrado) {
+	    esconderDialogo();
+	}
+	}
+        StartCoroutine(waiter()); //para esperar
+    } 
+
+    void esconderDialogo() {
+        panel.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,9 +51,17 @@ public class Trigersdetections : MonoBehaviour
             Destroy(other.gameObject);
             keys++;
             txtKeys.text = keys.ToString();
+            mostrarDialogo("Llave encontrada!", 2);
         }
     }
-
+/*
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Key") {
+            esconderDialogo();
+        }
+    }
+*/
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Puerta") {
@@ -40,12 +70,25 @@ public class Trigersdetections : MonoBehaviour
                 Destroy(collision.gameObject);
                 keys--;
                 txtKeys.text = keys.ToString();
+                mostrarDialogo("Has escapado de tu habitacion. Continua con cuidado.", 4);
             }
             else {
-                Debug.Log("No tines una llave para pasr por aqui");
+                Debug.Log("No tines una llave para pasar por aqui");
+                mostrarDialogo("Necesitas una llave para pasar por esta puerta!", 3);
             }
         }
-        
+        if (collision.collider.tag == "PuertaFinal") {
+	    mostrarDialogo("Has logrado escapar de la casa. Felicidades!", 4);
+	}
     }
-
+	/*
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Puerta") {
+            esconderDialogo();
+        }
+        if (collision.collider.tag == "PuertaFinal") {
+	    esconderDialogo();
+	}
+    }*/
 }
